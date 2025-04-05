9b23c1265f061466625b33e315925de400d8acac
Akshett Rai Jindal <jindalakshett@gmail.com>
2025-04-05 22:40:02 +0530

    feat(alienrj): wezterm wayland backend finally works!!

--- hosts/alienrj/options.nix
+++ hosts/alienrj/options.nix
@@ -179,7 +179,7 @@
           enable = true;
           use_official_package = false;
           font_size = 17;
-          enable_wayland = false;
+          enable_wayland = true;
         };
         alacritty = {
           enable = true;
