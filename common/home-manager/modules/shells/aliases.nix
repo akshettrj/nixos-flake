7ff1ebab2d2de1073714c03f3b3599f3a43d1412
Akshett Rai Jindal <jindalakshett@gmail.com>
2025-05-23 01:43:11 +0530

    feat(shell): add alias for home-manager commands

--- common/home-manager/modules/shells/aliases.nix
+++ common/home-manager/modules/shells/aliases.nix
@@ -5,4 +5,6 @@
   nh-switch = "nh os switch ~/.config/nixos-flake";
   nh-boot = "nh os boot ~/.config/nixos-flake";
   nh-build = "nh os build ~/.config/nixos-flake";
+  hm-switch = "home-manager switch --flake ~/.config/nixos-flake |& nom";
+  hm-news = "home-manager news --flake ~/.config/nixos-flake";
 }
