1aa889f5dec1543b5276d4f0fc01cc03f9a627b2
Akshett Rai Jindal <jindalakshett@gmail.com>
2025-05-26 23:18:18 +0530

    feat(raspi): enable UI

--- hosts/raspi/options.nix
+++ hosts/raspi/options.nix
@@ -1,4 +1,4 @@
-{...}: {
+{ inputs, pkgs, ...}: {
   propheci = {
     system = {
       hostname = "raspi";
@@ -60,7 +60,34 @@
     };
 
     # Appearance
-    theming.enable = false;
+    theming = {
+      enable = true;
+      fonts = {
+        nerdfonts = [
+          "jetbrains-mono"
+          "iosevka"
+        ];
+        main = {
+          name = "Iosevka NF";
+          size = 13;
+        };
+        backups = [
+          {
+            name = "JetBrainsMono NF";
+            size = 13;
+          }
+        ];
+      };
+      gtk = true;
+      qt = true;
+      cursor = {
+        package = pkgs.whitesur-cursors;
+        name = "WhiteSur-cursors";
+        size = 26;
+      };
+      minimum_brightness = 40;
+      wallpaper = "${inputs.wallpapers}/panda-2-1920×1080.png";
+    };
 
     dev = {
       git = {
@@ -76,7 +103,32 @@
     };
 
     programs = {
-      media.enable = false;
+      media = {
+        enable = true;
+        services.mpris.enable = true;
+        audio = {
+          mpd = {
+            enable = true;
+            ncmpcpp.enable = true;
+          };
+        };
+        video = {
+          mpv.enable = true;
+          vlc.enable = false;
+          stremio.enable = true;
+        };
+        picture = {
+          feh.enable = true;
+          sxiv.enable = true;
+        };
+        documents = {
+          zathura = {
+            enable = true;
+            useMupdf = true;
+          };
+          sioyek.enable = true;
+        };
+      };
       editors = {
         main = "neovim";
         backup = "helix";
@@ -90,20 +142,86 @@
         };
         zeditor.enable = false;
       };
-      terminals.enable = false;
-      browsers.enable = false;
+      terminals = {
+        enable = true;
+        main = "wezterm";
+        backup = "alacritty";
+        wezterm = {
+          enable = true;
+          use_official_package = true;
+          font_size = 10;
+          enable_wayland = false;
+        };
+        alacritty = {
+          enable = true;
+          font_size = 11;
+        };
+        ghostty.enable = false;
+      };
+      browsers = {
+        enable = true;
+        main = "brave";
+        brave = {
+          enable = true;
+          cmd_args = [];
+        };
+        firefox.enable = true;
+        chromium.enable = false;
+        chrome.enable = false;
+      };
       file_explorers = {
         main = "lf";
         backup = "yazi";
         lf.enable = true;
         yazi.enable = true;
       };
-      launchers.enable = false;
-      screenshot_tools.enable = false;
-      notification_daemons.enable = false;
-      clipboard_managers.enable = false;
-      bars.enable = false;
-      screenlocks.enable = false;
+      launchers = {
+        enable = true;
+        bemenu = {
+          enable = true;
+          font_size = 13;
+        };
+      };
+      screenshot_tools = {
+        enable = true;
+        flameshot.enable = true;
+        wayshot.enable = true;
+        shotman.enable = true;
+        hyprshot.enable = true;
+      };
+      screenlocks = {
+        enable = true;
+        swaylock.enable = true;
+        hyprlock = {
+          enable = false;
+          background_image = "${inputs.wallpapers}/gta-5-wallpaper-1920×1080.jpg";
+        };
+      };
+      clipboard_managers = {
+        enable = true;
+        copyq.enable = true;
+      };
+      notification_daemons = {
+        enable = true;
+        dunst = {
+          enable = true;
+          font_size = 10;
+        };
+      };
+      bars = {
+        enable = true;
+        waybar = {
+          enable = true;
+          use_official_package = false;
+          heights = 28;
+          font_size = 12;
+          separator_size = 18;
+          icon_size = 15;
+          tray_spacing = 8;
+          is_laptop = true;
+          systemd_target = "hyprland-session.target";
+        };
+      };
       extra_utilities = {
         drivedlgo.enable = true;
         rclone.enable = true;
@@ -123,6 +241,22 @@
       zoxide.enable = true;
     };
 
-    desktop_environments.enable = false;
+    desktop_environments = {
+      enable = true;
+      defaults = {
+        "/dev/tty1" = "hyprland";
+      };
+      wayland.enable = true;
+      hyprland = {
+        enable = true;
+        use_official_packages = true;
+        scroll_factor = 0.2;
+        launcher = "bemenu";
+        screenlock = "swaylock";
+        screenshot_tool = "hyprshot";
+        clipboard_manager = "copyq";
+        # monitors = [];
+      };
+    };
   };
 }
