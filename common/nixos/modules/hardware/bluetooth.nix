e8481c313df79f5ddc70f37198314c216737fa33
Akshett Rai Jindal <jindalakshett@gmail.com>
2025-12-14 15:39:27 +0530

    feat(bluetooth): spoof as apple device

--- common/nixos/modules/hardware/bluetooth.nix
+++ common/nixos/modules/hardware/bluetooth.nix
@@ -12,6 +12,7 @@
         enable = true;
         powerOnBoot = true;
         settings.General.Experimental = true;
+        settings.General.DeviceID = "bluetooth:004C:0000:0000";
       };
 
       services.blueman.enable = true;
